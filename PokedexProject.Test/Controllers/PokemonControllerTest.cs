7dcdd7ad2f8ede430b519907559818f0cf8020c2
mapokko <mapokko@hotmail.it>
2025-01-22 23:19:05 +0100

    fix: Add controller testing

---
+++ PokedexProject.Test/Controllers/PokemonControllerTest.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PokedexProject.Controllers;
+using PokedexProject.Middlewares;
+using PokedexProject.Middlewares.PokemonService;
+using PokedexProject.Middlewares.TranslationService;
+
+namespace PokedexProject.Test.Controllers
+{
+    public class PokemonControllerTest : IDisposable
+    {
+        private Mock<IPokemonService> _pokemonService;
+        private Mock<ITranslationService> _translationService;
+        private PokemonDisplay defaultPokemon;
+
+        public PokemonControllerTest()
+        {
+            _pokemonService = new Mock<IPokemonService>();
+            _translationService = new Mock<ITranslationService>();
+
+            defaultPokemon = new PokemonDisplay()
+            {
+                Description = "normal Description",
+                HabitatName = "swamp",
+                IsLegendary = true,
+                Name = "mewtwo"
+            };
+        }
+
+        public void Dispose()
+        {
+        }
+
+        [Fact]
+        public async void GetPokemon_ShouldReturnBadRequest_WhenNoPokemonGiven()
+        {
+            var pokemonController = new PokemonController(_pokemonService.Object, _translationService.Object);
+
+            var result = await pokemonController.GetPokemon("") as ObjectResult;
+
+            Assert.NotNull(result?.Value);
+            Assert.Equal(HttpStatusCode.BadRequest, (HttpStatusCode)result.StatusCode);
+
+            var errorList = result.Value as ErrorList;
+
+            Assert.Collection(errorList.Errors, error =>
+            {
+                Assert.Equal(error.ErrorMessage, "Pokemon name cannot be empty");
+            });
+
+
+        }
+
+        [Fact]
+        public async void GetTranslatedPokemon_ShouldReturnBadRequest_WhenNoPokemonGiven()
+        {
+            var pokemonController = new PokemonController(_pokemonService.Object, _translationService.Object);
+            var result = await pokemonController.GetTranslatedPokemon("") as ObjectResult;
+
+            Assert.NotNull(result?.Value);
+            Assert.Equal(HttpStatusCode.BadRequest, (HttpStatusCode)result.StatusCode);
+
+            var errorList = result.Value as ErrorList;
+
+            Assert.Collection(errorList.Errors, error =>
+            {
+                Assert.Equal(error.ErrorMessage, "Pokemon name cannot be empty");
+            });
+
+        }
+
+        [Fact]
+        public async void GetPokemon_ShouldReturnSuccess_WhenServiceMethodSucceeds()
+        {
+            _pokemonService.Setup(x => x.GetPokemonByInfo(It.IsAny<string>())).ReturnsAsync(Result<PokemonDisplay>.SuccessResult(defaultPokemon));
+            var pokemonController = new PokemonController(_pokemonService.Object, _translationService.Object);
+
+            var result = await pokemonController.GetPokemon("test") as OkObjectResult;
+
+            Assert.Equal(HttpStatusCode.OK, (HttpStatusCode)result.StatusCode);
+            Assert.NotNull(result.Value);
+
+            var value = result.Value as PokemonDisplay;
+            Assert.Equal(defaultPokemon.Description, value.Description);
+        }
+
+        [Fact]
+        public async void GetTranslatedPokemon_ShouldReturnSuccess_WhenServiceMethodSucceeds()
+        {
+            _translationService.Setup(x => x.GetTranslatedPokemonByInfo(It.IsAny<string>())).ReturnsAsync(Result<PokemonDisplay>.SuccessResult(defaultPokemon));
+            var pokemonController = new PokemonController(_pokemonService.Object, _translationService.Object);
+
+            var result = await pokemonController.GetTranslatedPokemon("test") as OkObjectResult;
+
+            Assert.Equal(HttpStatusCode.OK, (HttpStatusCode)result.StatusCode);
+            Assert.NotNull(result.Value);
+
+            var value = result.Value as PokemonDisplay;
+            Assert.Equal(defaultPokemon.Description, value.Description);
+        }
+
+        [Fact]
+        public async void GetPokemon_ShouldReturnError_WhenServiceMethodGivesError()
+        {
+            _pokemonService.Setup(x => x.GetPokemonByInfo(It.IsAny<string>())).ReturnsAsync(Result<PokemonDisplay>.ErrorResult(HttpStatusCode.InternalServerError, "Internal Server Error :)"));
+            var pokemonController = new PokemonController(_pokemonService.Object, _translationService.Object);
+
+            var result = await pokemonController.GetPokemon("test") as ObjectResult;
+
+            Assert.Equal(HttpStatusCode.InternalServerError, (HttpStatusCode)result.StatusCode);
+
+            var errorList = result.Value as ErrorList;
+
+            Assert.Collection(errorList.Errors, error =>
+            {
+                Assert.Equal(error.ErrorMessage, "Internal Server Error :)");
+            });
+
+        }
+
+        [Fact]
+        public async void GetTranslatedPokemon_ShouldReturnError_WhenServiceMethodGivesError()
+        {
+            _translationService.Setup(x => x.GetTranslatedPokemonByInfo(It.IsAny<string>())).ReturnsAsync(Result<PokemonDisplay>.ErrorResult(HttpStatusCode.InternalServerError, "Internal Server Error :)"));
+            var pokemonController = new PokemonController(_pokemonService.Object, _translationService.Object);
+
+            var result = await pokemonController.GetTranslatedPokemon("test") as ObjectResult;
+
+            Assert.Equal(HttpStatusCode.InternalServerError, (HttpStatusCode)result.StatusCode);
+
+            var errorList = result.Value as ErrorList;
+
+            Assert.Collection(errorList.Errors, error =>
+            {
+                Assert.Equal(error.ErrorMessage, "Internal Server Error :)");
+            });
+
+        }
+    }
+}
