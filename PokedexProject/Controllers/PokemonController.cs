087a5eb893feaa672244ab077fbf3167654361e3
mapokko <mapokko@hotmail.it>
2025-01-23 01:21:20 +0100

    chore: add readme and api comments

--- PokedexProject/Controllers/PokemonController.cs
+++ PokedexProject/Controllers/PokemonController.cs
@@ -8,11 +8,40 @@ using PokedexProject.Middlewares.TranslationService;
 
 namespace PokedexProject.Controllers
 {
-
     [Route("pokemon")]
     public class PokemonController(IPokemonService pokemonService, ITranslationService translationService) : ControllerBase
     {
-
+        /// <summary>
+        /// Searches information about the provided pokemon name.
+        /// </summary>
+        /// <param name="pokemonName">The name of the pokemon</param>
+        /// <returns>A newly created TodoItem</returns>
+        /// <remarks>
+        /// Sample response:
+        ///
+        ///     {
+        ///         "description": "Spits fire that is hot enough to melt boulders. Known to cause forest fires unintentionally.",
+        ///         "habitat": "mountain",
+        ///         "is_legendary": false,
+        ///         "name": "Charizard"
+        ///     }
+        ///
+        /// Sample Error:
+        /// 
+        ///     {
+        ///         "error_list":
+        ///         [
+        ///             {
+        ///                 "error_message": "Not Found"
+        ///             }
+        ///         ]
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="200">Returns the information about the pokemon</response>
+        /// <response code="400">If the pokemon name is missing</response>
+        /// <response code="404">If the item is not found or any of the 4 required attributes are not found</response>
+        /// <response code="500">For any internal error</response>
         [HttpGet("{pokemonName}")]
         public async Task<IActionResult> GetPokemon([FromRoute] string pokemonName)
         {
@@ -35,6 +64,37 @@ namespace PokedexProject.Controllers
             return objectResult;
         }
 
+        /// <summary>
+        /// Searches information about the provided pokemon name and translates the description using funtranslation's yoda or shakespeare apis'.
+        /// </summary>
+        /// <param name="pokemonName">The name of the pokemon</param>
+        /// <returns>A newly created TodoItem</returns>
+        /// <remarks>
+        /// Sample response:
+        ///
+        ///     {
+        ///         "name": "Charizard",
+        ///         "description": "Spits fire yond is hot enow to melt boulders. Known to cause forest fires unintentionally.",
+        ///         "is_legendary": false,
+        ///         "habitat": "mountain"
+        ///     }
+        ///
+        /// Sample Error:
+        /// 
+        ///     {
+        ///         "error_list":
+        ///         [
+        ///             {
+        ///                 "error_message": "Not Found"
+        ///             }
+        ///         ]
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="200">Returns the information about the pokemon alongside the translated description</response>
+        /// <response code="400">If the pokemon name is missing</response>
+        /// <response code="404">If the item is not found or any of the 4 required attributes are not found</response>
+        /// <response code="500">For any internal error</response>
         [HttpGet("translated/{pokemonName}")]
         public async Task<IActionResult> GetTranslatedPokemon([FromRoute] string pokemonName)
         {
